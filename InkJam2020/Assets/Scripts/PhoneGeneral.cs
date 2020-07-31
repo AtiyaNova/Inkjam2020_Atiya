0de8f768c672ef88061ed58fb3e47ecce4d441df
AtiyaNova <atiyanova@gmail.com>
2020-07-31 19:13:17 -0400

    [ADD] Phone Interface Beginnings
    The basic phone screens, along with the transitions between them

---
+++ InkJam2020/Assets/Scripts/PhoneGeneral.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhoneGeneral : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject lockScreen, homeScreen, appScreen;
+
+    //Singleton
+    public static PhoneGeneral instance;
+
+    void Awake()
+    {
+        if (instance == null) { instance = this; }
+        else { Destroy(gameObject); }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeScreens(true, false, false);
+    }
+
+    public void ChangeScreens(bool lockS, bool homeS, bool appS)
+    {
+        lockScreen.SetActive(lockS);
+        homeScreen.SetActive(homeS);
+        appScreen.SetActive(appS);
+    }
+}
---
+++ InkJam2020/Assets/Scripts/SwipeToUnlock.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SwipeToUnlock : MonoBehaviour
+     , IDragHandler
+     , IEndDragHandler
+     , IBeginDragHandler
+
+{
+    private Vector2 initPos;
+    [SerializeField]
+    private float finalX;
+    private RectTransform rectTransform;
+
+    private void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        initPos = rectTransform.anchoredPosition;
+    }
+
+    //Will probably use this for visual fx
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+    }
+    
+    //swipe is kinda busted, needs to be fixed
+    //Drags the swipe
+    public void OnDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    //Unlocks the screen
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        PhoneGeneral.instance.ChangeScreens(false, true, false);
+    }
+
+    private void ResetPosition()
+    {
+        rectTransform.anchoredPosition = initPos;
+    }
+}
