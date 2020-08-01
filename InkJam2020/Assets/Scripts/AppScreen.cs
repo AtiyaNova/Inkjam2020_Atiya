6a9a830c59740c7122b95493c9be8e94ab38bf32
AtiyaNova <atiyanova@gmail.com>
2020-08-01 03:29:13 -0400

    [ADD] Bulk of the menu traversal, and aesthetics

---
+++ InkJam2020/Assets/Scripts/AppScreen.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppScreen : MonoBehaviour
+{
+    //The pages
+    [SerializeField]
+    GameObject mainScreen, profileScreen;
+
+    //Singleton
+    public static AppScreen instance;
+
+    void Awake()
+    {
+        if (instance == null) { instance = this; }
+        else { Destroy(gameObject); }
+    }
+
+    private void Start()
+    {
+        mainScreen.SetActive(true);
+        profileScreen.SetActive(false);
+    }
+
+    public void TravelToProfile()
+    {
+        mainScreen.SetActive(false);
+        profileScreen.SetActive(true);
+    }
+
+    public void GoBack()
+    {
+        mainScreen.SetActive(true);
+        profileScreen.SetActive(false);
+    }
+
+    public void ConfirmCall()
+    {
+
+    }
+
+    public void CancelCall()
+    {
+
+    }
+
+    public void Call()
+    {
+
+    }
+
+    public void ExpandImage()
+    {
+
+    }
+
+    public void CloseImage()
+    {
+
+    }
+}
--- InkJam2020/Assets/Scripts/ClickApp.cs
+++ InkJam2020/Assets/Scripts/ClickApp.cs
@@ -1,13 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class ClickApp : MonoBehaviour
          , IPointerClickHandler
+         , IPointerEnterHandler
+         , IPointerExitHandler
 {
+    private RawImage appArt;
+    private Color orig, highlight;
+
+    private void Start()
+    {
+        appArt = GetComponent<RawImage>();
+        orig = appArt.color;
+        highlight = new Color(0.5f, 0.5f, 0.5f, 0.5f);
+    }
+
     public void OnPointerClick(PointerEventData eventData)
     {
         PhoneGeneral.instance.ChangeScreens(false, false, true);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        appArt.color = highlight;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        appArt.color = orig;
     }
 }
